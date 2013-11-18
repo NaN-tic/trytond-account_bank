03f15d401a55db12138228087fc609845015742f
resteve <resteve@zikzakmedia.com>
2013-11-18 09:03:27 +0100

    Change setup.py to use a better structure

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
-# This file is part of account_bank module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# encoding: utf-8
+#This file is part account_bank module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 from setuptools import setup
 import re
+import os
 import ConfigParser
+
+MODULE = 'account_bank'
+PREFIX = 'trytonspain'
+MODULE2PREFIX = {}
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -19,29 +29,33 @@ minor_version = int(minor_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+        prefix = MODULE2PREFIX.get(dep, 'trytond')
+        requires.append('%s_%s >= %s.%s, < %s.%s' %
+                (prefix, dep, major_version, minor_version,
+                major_version, minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytonspain_account_bank',
-    version=info.get('version', '2.8'),
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
+setup(name='%s_%s' % (PREFIX, MODULE),
+    version=info.get('version', '0.0.1'),
     description=('Tryton module to make payment/collection through bank '
         'accounts.'),
+    long_description=read('README'),
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
-    url='http://www.zikzakmedia.com',
-    download_url=("https://bitbucket.org/trytonspain/"
-                  "trytond-account_bank"),
-    package_dir={'trytond.modules.account_bank': '.'},
+    url='http://www.zikzakmedia.com/',
+    download_url='https://bitbucket.org/trytonspain/trytond-%s' % MODULE,
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.account_bank',
-        'trytond.modules.account_bank.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.account_bank': info.get('xml', []) \
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po'],
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -50,9 +64,15 @@ setup(name='trytonspain_account_bank',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'Intended Audience :: Manufacturing',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
+        'Natural Language :: Czech',
+        'Natural Language :: Dutch',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Natural Language :: German',
+        'Natural Language :: Russian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
@@ -64,8 +84,9 @@ setup(name='trytonspain_account_bank',
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_bank = trytond.modules.account_bank
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
-)
+    tests_require=tests_require,
+    )

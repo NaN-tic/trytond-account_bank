6095a73753fcb29376c85c09111fe9053e6a4282
Albert Cervera i Areny <albert@nan-tic.com>
2014-06-10 07:19:01 +0200

    Default value for with_on_change parameter must be True.

--- account.py
+++ account.py
@@ -376,7 +376,7 @@ class CompensationMoveStart(ModelView, BankMixin):
         return pool.get('ir.date').today()
 
     @classmethod
-    def default_get(cls, fields, with_rec_name=True, with_on_change=False):
+    def default_get(cls, fields, with_rec_name=True, with_on_change=True):
         pool = Pool()
         Line = pool.get('account.move.line')
         PaymentType = pool.get('account.payment.type')

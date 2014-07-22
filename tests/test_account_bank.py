f206111b70b87e09a7c3e0f46d64d5b1363f47eb
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-07-22 13:01:56 +0200

    Cleanup tests

--- tests/test_account_bank.py
+++ tests/test_account_bank.py
@@ -2,48 +2,25 @@
 # This file is part of account_bank module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import os
-import sys
+from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
 import trytond.tests.test_tryton
 import unittest
 import doctest
-from trytond.backend.sqlite.database import Database as SQLiteDatabase
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class AccountBankTestCase(unittest.TestCase):
-    '''
-    Test Account Bank module.
-    '''
+    'Test Account Bank module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('account_bank')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('account_bank')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
-
-
-def doctest_dropdb(test):
-    database = SQLiteDatabase().connect()
-    cursor = database.cursor(autocommit=True)
-    try:
-        database.drop(cursor, ':memory:')
-        cursor.commit()
-    finally:
-        cursor.close()
 
 
 def suite():
@@ -54,6 +31,3 @@ def suite():
             setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())

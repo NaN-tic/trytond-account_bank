22802df2807ebf270e66a77b077acd2c8b11f480
resteve <resteve@zikzakmedia.com>
2015-12-01 10:45:15 +0100

    Test Scenerario

--- tests/test_account_bank.py
+++ tests/test_account_bank.py
@@ -2,8 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountBankTestCase(ModuleTestCase):
@@ -15,4 +17,7 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountBankTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_compensation_move.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
